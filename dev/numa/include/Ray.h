25bf5421b5522cac64f4f5806b0228fd359f7612
serg <a.sergey9758@gmail.com>
2025-02-01 18:50:46 +0300

    Added Ray class.

---
+++ dev/numa/include/Ray.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Vec3.hpp"
+
+namespace numa
+{
+	class Ray
+	{
+	public:
+
+		Ray() = default;
+		Ray(const Vec3& origin, const Vec3& direction);
+
+		numa::Vec3 GetPoint(float distance) const;
+
+		const Vec3& GetOrigin() const;
+		const Vec3& GetDirection() const;
+
+	private:
+
+		Vec3 origin{};
+		Vec3 direction{};
+	};
+}
