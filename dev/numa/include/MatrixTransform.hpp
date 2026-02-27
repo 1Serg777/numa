f6165ccaf78dcb012dfecacabfe1edd00655b0b5
serg <a.sergey9758@gmail.com>
2026-02-27 23:59:36 +0300

    Added the 'Scale' and 'Translate' functions to the 'MatrixTransform.hpp' file

--- dev/numa/include/MatrixTransform.hpp
+++ dev/numa/include/MatrixTransform.hpp
@@ -8,7 +8,18 @@
 namespace numa {
 
 	template<typename T>
-<<<<<<< HEAD
+	Mat<T, 4, 4> Scale(const Mat<T, 4, 4>& frame, const numa::Vec<T, 3>& scale) {
+		return Mat<T, 4, 4>{
+			frame[0] * scale.x,
+			frame[1] * scale.y,
+			frame[2] * scale.z,
+			frame[3]
+		};
+	}
+
+	// Yaw-Pitch-Roll Rotation
+
+	template<typename T>
 	Mat<T, 3, 3> RotateYawPitchRoll(const numa::Vec<T, 3>& rot) {
 		return RotateYaw(rot.y) * RotatePitch(rot.x) * RotateRoll(rot.z);
 	}
@@ -36,46 +47,20 @@ namespace numa {
 			Vec<T, 3>{-std::sin(roll), std::cos(roll), 0}, // column #1
 			Vec<T, 3>{0, 0, 1},                            // column #2
 		};
-=======
-	Mat<T, 3, 3> EulerAnglesPitch(const T& pitch) {
-		return Mat<T, 3, 3>{
-			// Columns
-			numa::Vec<T, 3>{1.0, 0.0, 0.0},
-			numa::Vec<T, 3>{0, std::cos(pitch), std::sin(pitch)},
-			numa::Vec<T, 3>{0, -std::sin(pitch), std::cos(pitch)}
-		};
-	}
-	template<typename T>
-	Mat<T, 3, 3> EulerAnglesYaw(const T& yaw) {
-		return Mat<T, 3, 3>{
-			// Columns
-			numa::Vec<T, 3>{std::cos(yaw), 0, -std::sin(yaw)},
-			numa::Vec<T, 3>{0, 1, 0},
-			numa::Vec<T, 3>{std::sin(yaw), 0, std::cos(yaw)}
-		};
-	}
-	template<typename T>
-	Mat<T, 3, 3> EulerAnglesRoll(const T& roll) {
-		return Mat<T, 3, 3>{
-			// Columns
-			numa::Vec<T, 3>{std::cos(roll), std::sin(roll), 0},
-			numa::Vec<T, 3>{-std::sin(roll), std::cos(roll), 0},
-			numa::Vec<T, 3>{0, 0, 1}
-		};
 	}
 
+	// Angle-Axis Rotation
+	// TODO
+
 	template<typename T>
-	Mat<T, 3, 3> EulerAnglesRotate(const T& pitch, const T& yaw, const T& roll) {
-		numa::Mat3 p = EulerAnglesPitch(pitch);
-		numa::Mat3 y = EulerAnglesYaw(yaw);
-		numa::Mat3 r = EulerAnglesRoll(roll);
-		numa::Mat3 R = y * p * r;
-		return R;
-	}
-	template<typename T>
-	Mat<T, 3, 3> EulerAnglesRotate(const Vec<T, 3>& rot) {
-		return EulerAnglesRotate(rot.x, rot.y, rot.z);
->>>>>>> f61165a938eefe285d731160fa039edbf9ec584e
+	Mat<T, 4, 4> Translate(const Mat<T, 4, 4>& frame, const numa::Vec<T, 3>& translate) {
+		return Mat<T, 4, 4>{
+			frame[0],
+			frame[1],
+			frame[2],
+			// frame[3] + numa::Vec<T, 4>(translate, 0)
+			frame * numa::Vec<T, 4>(translate, 1)
+		};
 	}
 
 }
